232613c660a5881d6dbd9b125261fdaf0debd21b
facundo-uribe-bigrun <124629894+facundo-uribe-bigrun-2@users.noreply.github.com>
2023-02-26 15:04:36 -0300

    Added a large part of the Unity AR Foundation Samples Repo including configurations and dependencies (v4.2.7 at commit:849db1c)
    Repo can be found at: https://github.com/Unity-Technologies/arfoundation-samples

--- UnityProject/Assets/Scripts/Boot.cs
+++ UnityProject/Assets/Scripts/Boot.cs
@@ -10,16 +10,42 @@ namespace Dedalord.LiveAr
     public class Boot : MonoBehaviour
     {
         /// <summary>
-        /// Scene to go to when testing live2d basic functionality.
+        /// Scene to go to when testing basic Live2D functionality.
         /// </summary>
         public string Live2DTestScene;
         
         /// <summary>
-        /// Unity Start.
+        /// Scene to go to when testing Unity AR Foundation functionality.
         /// </summary>
-        private void Start()
+        public string ARSamplesScene;
+        
+        /// <summary>
+        /// Scene to go to when returning to the starting scene
+        /// </summary>
+        public string RootScene;
+        
+        /// <summary>
+        /// Invoke to discard everything and go to the Live2D test scene
+        /// </summary>
+        public void OnLive2D()
         {
             SceneManager.LoadScene(Live2DTestScene);
+        }
+        
+        /// <summary>
+        /// Invoke to discard everything and go to the AR samples scene
+        /// </summary>
+        public void OnARSamples()
+        {
+            SceneManager.LoadScene(ARSamplesScene);
+        }
+        
+        /// <summary>
+        /// Invoke to discard everything and go to the starting scene
+        /// </summary>
+        public void OnGoBackToRoot()
+        {
+            SceneManager.LoadScene(RootScene);
         }
     }
 }
